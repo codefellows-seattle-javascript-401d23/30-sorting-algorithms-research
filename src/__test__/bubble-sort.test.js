66f150de6b38cbb0f756f2317d9fe83f2c1bcb0c
vibecastor <vibecastor@gmail.com>
2018-05-28 12:27:51 -0700

    project setup

---
+++ src/__test__/bubble-sort.test.js
@@ -0,0 +1,16 @@
+import bubbleSort from '../bubble-sort';
+import insertionSort from '../insertion-sort';
+
+const array = [10, 3, 7, 2, 8, 9, 5, 4, 1, 6];
+
+describe('Testing #bubbleSort on an array', () => {
+  test('#bubbleSort should return a sorted array with the values in ascending order', () => {
+    expect(bubbleSort(array)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+});
+
+describe('Testing #insertionSort on an array', () => {
+  test('#insertionSort should return a sorted array with the values in ascending order', () => {
+    expect(insertionSort(array)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+});
---
+++ src/bubble-sort.js
@@ -0,0 +1,16 @@
+import swap from './swap';
+
+export default function bubbleSort(array) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] && array[i + 1] && array[i] > array[i + 1]) {
+        swap(array, i, i + 1);
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return array;
+}
+
