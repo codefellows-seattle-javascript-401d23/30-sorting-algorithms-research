7e4b90c1c183a216eb78c2bd33a4214a89586de6
sarasaurus <bixler.s.e@gmail.com>
2018-05-28 15:12:14 -0700

    pushing more scaffold linter is being weird

--- src/bubble-sort.js
+++ src/bubble-sort.js
@@ -1,11 +1,13 @@
 'use strict';
-import bubbleSort from 'bubble-sort';
+const bubbleSort = (array) => {
 
-describe('testing Bubble Sort', ()=> {
-  test('it should sort in ascending order', () =>{
-    const testArray = [5,9,4,1,3,2,6,7,8];
-    const sortedArray = [1,2,3,4,5,6,7,8,9];
-    bubbleSort(testArray);
-    expect(testArray).toEqual(sortedArray)
-  })
-})
+  if (!array.length) {
+    throw new Error ('the array must be populated')
+  } else {
+    for (let i = 0; i<array.length; i++) {
+      
+    }
+  }
+
+}
+export default bubbleSort;  
