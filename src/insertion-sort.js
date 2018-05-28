4e9f6688eaccac1a5c01c4b579c38404db2754c1
sarasaurus <bixler.s.e@gmail.com>
2018-05-28 16:46:40 -0700

    insertion is working

--- src/insertion-sort.js
+++ src/insertion-sort.js
@@ -1,26 +1,23 @@
 'use strict';
 
 const insertionSort = (array) => {
-  console.log('before:', array);
-  const _swap = (thisArray, index1, index2) => {
-    const storedValue = thisArray[index1];
-    thisArray[index1] = thisArray[index2];
-    thisArray[index2] = storedValue;
-    return undefined;
-  };
   if (!array.length) {
     throw new Error('the array must be populated');
   } else {
-    const length = array.length + 1;
-    for (let i = length; i >= 0; i--) {
-      for (let j = 0; j <= length; j++) {
-        if (array[j - 1] > array[j]) {
-          _swap(array, j - 1, j);
-        }
+    const { length } = array;
+    let iterator = 1;
+    while (iterator < length) {
+      const value = array[iterator];
+      let hindIterator = iterator - 1;
+      while (hindIterator >= 0 && array[hindIterator] > value) {
+        array[hindIterator + 1] = array[hindIterator];
+        hindIterator -= 1;
       }
+      array[hindIterator + 1] = value;
+      iterator += 1;
     }
   }
-  console.log('after:', array);
-  return undefined;
 };
+
 export default insertionSort;
+
