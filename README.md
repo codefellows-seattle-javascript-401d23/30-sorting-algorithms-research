cf6aa6943c8986ded5caf84100a39b07f3af45f3
dahby <dstoll243@gmail.com>
2018-05-29 08:23:19 -0700

    readme updated

---
+++ README.md
@@ -0,0 +1,25 @@
+## Lab 30: Sorting Algorithms Research
+
+### Bubble Sort
+
+`bubbleSort(array)`
+- Arity of 1
+- Takes in an array of numbers
+- If no array passed, will return `undefined`
+- Will return array values in place in sequential order
+
+### Insertion Sort
+
+`insertionSort(array)`
+- Arity of 1
+- Takes in an array of numbers
+- If no array passed, will return `undefined`
+- Will return array values in place in sequential order
+
+### Selection Sort
+
+`selectionSort(array)`
+- Arity of 1
+- Takes in an array of numbers
+- If no array passed, will return `undefined`
+- Will return array values in place in sequential order
--- src/__test__/insert.test.js
+++ src/__test__/insert.test.js
@@ -1,13 +1,13 @@
 'use strict';
 
-import insertSort from '../lib/insert';
+import insertionSort from '../lib/insert';
 
-describe('#insertSort', () => {
-  test('Should insert sort array in sequential order', () => {
-    expect(insertSort([4, 6, 2, 1])).toEqual([1, 2, 4, 6]);
-    expect(insertSort([5, 23, -52, -1, 7])).toEqual([-52, -1, 5, 7, 23]);
+describe('#insertionSort', () => {
+  test('Should insertion sort array in sequential order', () => {
+    expect(insertionSort([4, 6, 2, 1])).toEqual([1, 2, 4, 6]);
+    expect(insertionSort([5, 23, -52, -1, 7])).toEqual([-52, -1, 5, 7, 23]);
   });
   test('Should return undefined if no array is passed', () => {
-    expect(insertSort()).toBeUndefined();
+    expect(insertionSort()).toBeUndefined();
   });
 });
