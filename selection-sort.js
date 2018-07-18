80ec295484142f50bf5fc1c0a636158006628494
Joanna Coll <35988918+johannam999@users.noreply.github.com>
2018-07-18 13:11:29 -0700

    Update selection-sort.js

--- selection-sort.js
+++ selection-sort.js
@@ -4,20 +4,19 @@ const array =[10,5,34,56,2,4,9];
 
 function selectionSort(array) {
 var len = array.length;
-	for (var i = 0; i < len-1; i++) {
+  for (var i = 0; i < len-1; i++) {
     let minValue = i;
-    for (var j = i + 1; j < len; j++) {
-      
+    for (var j = i + 1; j < len; j++) {  
       if(array[j] < array[minValue]) {
         minValue = j;
       }
-		}
+    }
     if (minValue !== i) {
       let tempVal = array[i];
       array[i] = array[minValue];
       array[minValue] = tempVal;
     }
-	}
+  }
   return array;
 }
 
