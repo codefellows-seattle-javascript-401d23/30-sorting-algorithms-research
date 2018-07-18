4a707f6212b235ce9115458d920b7dbaa343524a
Joanna Coll <35988918+johannam999@users.noreply.github.com>
2018-07-17 17:17:07 -0700

    Update insertion-sort.js

--- insertion-sort.js
+++ insertion-sort.js
@@ -3,14 +3,14 @@
 const array =[10,5,34,56,2,4,9];
 
 function insertionSort(array) {
-let len = array.length;
-	for (let i = 1; i < len; i++) {
-		let sorted = array[i];
-		for (let j = i - 1; j >= 0 && (array[j] > sorted); j--) {
-			array[j + 1] = array[j];
-		}
-		array[j + 1] = sorted;
-	}
+  let len = array.length;
+    for (let i = 1; i < len; i++) {
+      let temp = array[i];
+      for (let j = i - 1; j >= 0 && (array[j] > temp); j--) {
+	array[j + 1] = array[j];
+	array[j] = temp;
+      }     
+}
   return array;
 }
 
